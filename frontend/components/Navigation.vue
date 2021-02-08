4566e1d45b3567238a3c16d74c2b4644863578ed
Patricija Brečko <patricija.brecko@gmail.com>
2021-02-08 11:34:01 +0100

    predlogi folder structure & navigation component

---
+++ frontend/components/Navigation.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-container fluid>
+    <b-row class="navigation justify-content-between align-items-center">
+      <img src="~/assets/img/izboljsajmo_maribor_logo.png" alt="">
+      <div class="">
+        <NuxtLink to="/">
+          Domov
+        </NuxtLink>
+        <NuxtLink to="/">
+          O izboljšajmo Maribor
+        </NuxtLink>
+        <NuxtLink to="/">
+          Pomoč
+        </NuxtLink>
+        <NuxtLink class="login" to="/login">
+          Prijava
+        </NuxtLink>
+      </div>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="scss">
+  .navigation {
+    padding: 1rem 2rem;
+    box-shadow: 3px 3px 7px #d4d9e1, -3px -3px 7px #ffffff;
+    background-color: #f8f8f8;
+
+    img {
+      height: 40px;
+    }
+
+    a {
+      color: #606060;
+      margin-left: 1rem;
+      padding: 0.5rem 1rem;
+
+      &:hover {
+        text-decoration: none;
+        color: #ef7782;
+      }
+
+      &.login {
+        background-color: rgba(239, 119, 130, 0.3);
+        border-radius: 1rem;
+      }
+    }
+  }
+</style>
