b022693af3bb1b7d606656c003b02f2d73fc3944
patricijab <patricija.brecko@gmail.com>
2021-03-31 10:06:32 +0200

    success and error messages for posting comments and initiatives

--- frontend/pages/predlogi/oddaj/moti-me.vue
+++ frontend/pages/predlogi/oddaj/moti-me.vue
@@ -1,92 +1,27 @@
 <template>
-  <b-container fluid>
-    <b-row class="justify-content-center">
-      <b-col cols="12" md="10" lg="6" class="position-relative">
-        <div>
-          <div
-            class="form-top form-top-mm"
-          />
-          <div class="initiative-form">
-            <h4>MOTI ME</h4>
-            <p class="form-subtitle">
-              Sporočite nam, če ste v svojem okolju zaznali kaj, kar vas moti (okvare, poškodbe, pomanjkljivosti, nepravilnosti …) in menite, da je v pristojnosti MO Maribor, da to reši.
-            </p>
-            <Initiative
-              :descriptions="descriptions"
-              :error-draft="errorDraft"
-              @create-initiative="createInitiative"
-              @create-draft="createDraft"
-            />
-          </div>
-        </div>
-      </b-col>
-    </b-row>
-  </b-container>
+  <NewInitiative
+    :title="title"
+    :subtitle="subtitle"
+    :type="type"
+    :descriptions="descriptions"
+  />
 </template>
 
 <script>
-import Initiative from '~/components/forms/InitiativeForm'
+import NewInitiative from '~/components/NewInitiative'
 
 export default {
-  components: { Initiative },
+  components: { NewInitiative },
   middleware: 'auth',
   data () {
     return {
+      title: 'MOTI ME',
+      subtitle: 'Sporočite nam, če ste v svojem okolju zaznali kaj, kar vas moti (okvare, poškodbe, pomanjkljivosti, nepravilnosti …) in menite, da je v pristojnosti MO Maribor, da to reši.',
       type: 'MM',
       descriptions: [
         'Na kratko opišite, kaj vas moti. Zakaj? Če je mogoče, opišite svojo izkušnje, da nam pomagate bolje razumeti vašo pripombo.',
         'Kako bi lahko to, kar vas moti, popravili?'
-      ],
-      errorDraft: false
-    }
-  },
-  computed: {
-  },
-  methods: {
-    async createDraft (form, id) {
-      this.errorDraft = false
-      try {
-        // is draft
-        form.is_draft = true
-        console.log('draft', form)
-        if (id < 0) { // draft does not exist yet
-          // add initiative type
-          form.type = this.type
-          id = await this.$store.dispatch('postInitiative', form)
-        } else {
-          id = await this.$store.dispatch('patchInitiative', { form, id })
-        }
-        if (id < 0) {
-          this.errorDraft = true
-        } else {
-          await this.$router.push('/')
-        }
-      } catch (err) {
-        // this.errorComment = true
-        console.log(err)
-      }
-    },
-    async createInitiative (form, id) {
-      try {
-        // is draft
-        form.is_draft = false
-        // add initiative type
-        form.type = this.type
-        console.log('publish', form)
-        if (id < 0) { // draft does not exist yet
-          id = await this.$store.dispatch('postInitiative', form)
-        } else {
-          id = await this.$store.dispatch('patchInitiative', { form, id })
-        }
-        if (id < 0) {
-          this.errorDraft = true
-        } else {
-          await this.$router.push(`/predlogi/${id}`)
-        }
-      } catch (err) {
-        // this.errorComment = true
-        console.log(err)
-      }
+      ]
     }
   }
 }
--- frontend/pages/predlogi/oddaj/zanima-me.vue
+++ frontend/pages/predlogi/oddaj/zanima-me.vue
@@ -1,92 +1,26 @@
 <template>
-  <b-container fluid>
-    <b-row class="justify-content-center">
-      <b-col cols="12" md="10" lg="6" class="position-relative">
-        <div>
-          <div
-            class="form-top form-top-zm"
-          />
-          <div class="initiative-form">
-            <h4>ZANIMA ME</h4>
-            <p class="form-subtitle">
-              Vprašajte, kar vas zanima.
-            </p>
-            <Initiative
-              :descriptions="descriptions"
-              :error-draft="errorDraft"
-              @create-initiative="createInitiative"
-              @create-draft="createDraft"
-            />
-          </div>
-        </div>
-      </b-col>
-    </b-row>
-  </b-container>
+  <NewInitiative
+    :title="title"
+    :subtitle="subtitle"
+    :type="type"
+    :descriptions="descriptions"
+  />
 </template>
 
 <script>
-import Initiative from '~/components/forms/InitiativeForm'
+import NewInitiative from '~/components/NewInitiative'
 
 export default {
-  components: { Initiative },
+  components: { NewInitiative },
   middleware: 'auth',
   data () {
     return {
+      title: 'ZANIMA ME',
+      subtitle: 'Vprašajte, kar vas zanima.',
       type: 'ZM',
       descriptions: [
         'Kaj vas zanima? Vaše vprašanje naj bo jasno in nedvoumno; če je potrebno in če je mogoče, na kratko opišite okoliščine, na katere se nanaša vaše vprašanje. Tako vam bomo lažje podali kvaliteten in konkreten odgovor.'
-      ],
-      errorDraft: false
-    }
-  },
-  computed: {
-  },
-  methods: {
-    async createDraft (form, id) {
-      this.errorDraft = false
-      try {
-        // is draft
-        form.is_draft = true
-        console.log('draft', form)
-        if (id < 0) { // draft does not exist yet
-          // add initiative type
-          form.type = this.type
-          id = await this.$store.dispatch('postInitiative', form)
-        } else {
-          id = await this.$store.dispatch('patchInitiative', { form, id })
-        }
-        if (id < 0) {
-          this.errorDraft = true
-          console.log('error create draft')
-        } else {
-          await this.$router.push('/')
-        }
-      } catch (err) {
-        // this.errorComment = true
-        console.log(err)
-      }
-    },
-    async createInitiative (form, id) {
-      try {
-        // is draft
-        form.is_draft = false
-        // add initiative type
-        form.type = this.type
-        console.log('publish', form)
-        if (id < 0) { // draft does not exist yet
-          id = await this.$store.dispatch('postInitiative', form)
-        } else {
-          id = await this.$store.dispatch('patchInitiative', { form, id })
-        }
-        if (id < 0) {
-          this.errorDraft = true
-        } else {
-          await this.$router.push(`/predlogi/${id}`)
-        }
-      } catch (err) {
-        // this.errorComment = true
-        console.log(err)
-      }
+      ]
     }
   }
 }
