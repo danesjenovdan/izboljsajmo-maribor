25c6953a002740d27a3186667df9914133a3439c
patricijab <patricija.brecko@gmail.com>
2021-04-07 15:42:31 +0200

    pogoji uporabe

--- frontend/pages/pogoji-uporabe.vue
+++ frontend/pages/pogoji-uporabe.vue
@@ -1,7 +1,107 @@
 <template>
-  <div>
-    Pogoji uporabe
-  </div>
+  <b-container>
+    <b-row class="mt-5">
+      <b-col>
+        <h1>
+          Splošni pogoji uporabe storitve »Izboljšajmo Maribor«
+        </h1>
+        <h2>
+          I. Splošno o uporabi storitve
+        </h2>
+        <p>
+          Splošni pogoji uporabe storitve »Izboljšajmo Maribor« določajo pogoje, na podlagi katerih se uporabniku omogoča uporaba predmetne storitve.
+        </p>
+        <p>
+          Upravljavec storitve »Izboljšajmo Maribor« je Mestna občina Maribor (v nadaljevanju: upravljavec).
+        </p>
+        <p>
+          Namen storitve »Izboljšajmo Maribor« je s pomočjo sodobne tehnologije vzpostaviti sistem, ki omogoča enostavno posredovanje pobud in vprašanj, ki se nanašajo na življenje v Mestni občini Maribor. Mestna občina Maribor podaja odgovore na pobude v delokrogu svojih pristojnosti.
+        </p>
+        <h2>
+          II. Upravljanje in dostopnost do storitve
+        </h2>
+        <p>
+          Uporabnik lahko do storitve na spletnem naslovu: <a href="https://izboljsajmo.maribor.si/">https://izboljsajmo.maribor.si/</a>
+        </p>
+        <p>
+          Sistem zahteva prijavo uporabnika v primeru, če želi ta oddati pobudo ali vprašanje, ali glasovati. Vsaka pobuda ali vprašanje uporabnikov se pred objavo pregleda s strani administratorja. Če je neprimerna se zavrže in se ne objavi.
+        </p>
+        <p>
+          Za prikaz določenih funkcionalnosti se uporabljajo neodvisne zunanje storitve in aplikacije. Za uporabo teh storitev veljajo pogoji uporabe zunanje storitve, ki jih določa lastnik storitve ali aplikacije. Za prikaz lokacije se uporablja storitve Google Maps (Google Zemljevidi), katerih pogoji uporabe so uporabniku na voljo na spletnem naslovu:
+          <a href="https://www.google.com/intl/en_si/help/terms_maps.html">https://www.google.com/intl/en_si/help/terms_maps.html</a>
+        </p>
+        <h2>
+          III. Varstvo podatkov
+        </h2>
+        <p>
+          Upravljavec storitve zagotavlja, da v skladu z veljavnim Zakonom o varstvu osebnih podatkov (ZVOP), varuje osebne podatke uporabnikov storitve. Ime in priimek uporabnika, ki je objavil pobudo ali vprašanje, sta javno vidna vsem uporabnikom storitev.
+        </p>
+        <p>
+          Upravljavec zbira, hrani in obdeluje osebne podatke uporabnikov le za namene storitve in le v času njenega delovanja.
+        </p>
+        <p>
+          Odgovori na prejete pobude in vprašanja se zagotavljajo skladno z navodili upravljavca, ki definirajo delovne procese in oblike vsebin, povezanih z objavami na svetovnem spletu.
+        </p>
+        <h2>
+          IV. Odgovornost upravljavca storitve
+        </h2>
+        <p>
+          Upravljavec storitve se obvezuje, da bo v dobri veri in v okviru svojih zmožnosti skrbel za pravilnost in ažurnost objavljenih vsebin na spletnih straneh, s tem pa ne izključuje možnosti za pojav morebitnih napak med objavljenimi vsebinami. Informacije posredovane prek tega spletnega mesta so tako zgolj informativne narave, saj upravljavec storitev in druga podjetja, ne prevzemajo odgovornosti za njihovo točnost ali morebitno škodo, ki bi uporabniku nastala zaradi uporabe storitve.
+        </p>
+        <p>
+          Upravljavec si pridržuje pravico, da v primeru pomanjkljive pobude ali vprašanja, uporabnika preko e-pošte pozove, da poda dodatno pojasnilo.
+        </p>
+        <p>
+          Upravljavec je odgovoren za vsebino zapisov prijavljenih piscev pobud ali vprašanj, zato si, skladno z veljavno zakonodajo in stališčem Evropskega sodišča za človekove pravice, pridržuje pravico do zavrnitve neprimernih, podcenjujočih in žaljivih komentarjev uporabnikov.
+        </p>
+        <p>
+          Vse fotografije, ki so sestavni del pobud ali vprašanj in so objavljene v predmetni storitvi, so predmet avtorske zaščite ali druge oblike zaščite intelektualne lastnine. Upravljavec je skladno z veljavnim Zakonom o avtorski in sorodnih pravicah, nosilec materialnih avtorskih pravic nad vsebinami objavljenimi v storitvi.
+        </p>
+        <p>
+          Storitev “Izboljšajmo Maribor”, ne nadomešča upravnih postopkov, ki jih vodijo pristojni organi ali upravljavec oz. podjetje v javnem interesu in so določeni z veljavnimi zakoni.
+          Upravljavec zagotavlja, da bo odgovore na pobude in vprašanja poskušal zagotoviti v največ pet delovnih dneh od prejema.
+        </p>
+        <p>
+          Ne glede na prejšnji odstavek, pobuda ali vprašanje uporabnika ne zavezuje upravljavca k obravnavi ali sanaciji pobude, saj je ta zgolj informativne narave.
+        </p>
+        <h2>
+          V. Kršitve splošnih pogojev uporabe
+        </h2>
+        <p>
+          Upravljavec ima pravico, da v primeru nedovoljene uporabe predmetne storitve in/ali kakršnih koli drugih zlorab pri dostopu oz. uporabi s strani uporabnikov ali katerih koli drugih oseb in/ali v primeru suma zlorabe prijave ali gesla, izvede vse ukrepe in druga dejanja, ki jih v skladu s svojo lastno presojo šteje za potrebne zaradi varstva svojih pravic in interesov uporabnikov in/ali zaradi varstva pravic in interesov katerih koli drugih oseb.
+        </p>
+        <p>
+          Po 297. členu veljavnega Kazenskega zakonika je uporabnik kazensko odgovoren za javno spodbujanje sovraštva, nasilja ali nestrpnosti.
+        </p>
+        <h2>
+          VI. Pravica do spremembe splošnih pogojev uporabe
+        </h2>
+        <p>
+          Upravljavec ima pravico, da te pogoje kadar koli spremeni in o spremembah obvesti uporabnike na običajno zanesljiv način (z objavo).
+        </p>
+        <p>
+          Spremembe zavezujejo uporabnike z dnem objave.
+        </p>
+        <h2>
+          VII. Reševanje sporov
+        </h2>
+        <p>
+          Spore nastale v povezavi s temi splošnimi pogoji, bodo stranke reševale sporazumno. Če spora ni možno rešiti po mirni poti, je krajevno pristojno sodišče po sedežu upravljavca storitve.
+        </p>
+        <h2>
+          VIII. Končne določbe
+        </h2>
+        <p>
+          Šteje se, da je uporabnik s prijavo in uporabo storitve “Izboljšajmo Maribor”, seznanjen z vsebino teh splošnih pogojev in z njimi soglaša.
+        </p>
+        <p class="my-5">
+          Splošni pogoji veljajo od dneva objave.<br>
+          Upravljavec storitve:<br>
+          Mestna občina Maribor
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -9,6 +109,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+h1 {
+  font-weight: 700;
+  margin-bottom: 2rem;
+}
 
+p {
+  font-size: 1.25rem;
+  font-weight: 300;
+  line-height: 1.5;
+}
 </style>
