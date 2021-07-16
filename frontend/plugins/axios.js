00761147267510d528aa0ea420bdca5c64540a0d
patricijab <patricija.brecko@gmail.com>
2021-07-16 13:49:02 +0200

    refresh token fix for frontend

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,20 +1,30 @@
-export default function ({ redirect, $axios, store }) {
-  $axios.onError((error) => {
-    const code = parseInt(error.response && error.response.status)
-    if (code === 401) {
-      store.$auth.strategy.token.reset()
-      $axios.post('auth/token/', {
-        refresh_token: store.$auth.strategy.refreshToken.get(),
-        client_id: store.state.client_id,
-        client_secret: store.state.client_secret,
-        grant_type: 'refresh_token'
-      }).then((response) => {
-        store.$auth.setUserToken(response.data.access_token, response.data.refresh_token).then(() => {
-          store.$router.go()
-        })
-      }).catch(() => {
-        store.$auth.logout()
-      })
-    }
+import createAuthRefreshInterceptor from 'axios-auth-refresh'
+
+export default function ({ $axios, store }) {
+  const instance = $axios.create()
+  delete instance.defaults.headers.common.Authorization
+  // Function that will be called to refresh authorization
+  const refreshAuthLogic = failedRequest => instance.post('auth/token/', {
+    refresh_token: store.$auth.strategy.refreshToken.get(),
+    client_id: store.state.client_id,
+    client_secret: store.state.client_secret,
+    grant_type: 'refresh_token'
   })
+    .then((tokenRefreshResponse) => {
+      store.$auth.setUserToken(tokenRefreshResponse.data.access_token, tokenRefreshResponse.data.refresh_token).then(() => {
+        failedRequest.response.config.headers.Authorization = 'Bearer ' + tokenRefreshResponse.data.token
+        return Promise.resolve()
+      })
+    }).catch(() => {
+      store.$auth.logout()
+    })
+
+  // by default intercepts 401
+  createAuthRefreshInterceptor(
+    $axios,
+    refreshAuthLogic,
+    {
+      pauseInstanceWhileRefreshing: true
+    }
+  )
 }
